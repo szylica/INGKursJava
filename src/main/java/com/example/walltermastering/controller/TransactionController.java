e45f3e8a68ea32d563652f5bcde5a5acbd22c02f
Szymon Stalica <szymonstalica@MacBook-Air-Szymon.local>
2026-06-01 08:55:46 +0200

    feat: initial commit

---
+++ src/main/java/com/example/walltermastering/controller/TransactionController.java
@@ -0,0 +1,4 @@
+package com.example.walltermastering.controller;
+
+public class TransactionController {
+}
---
+++ src/test/java/com/example/walltermastering/WallterMasterIngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walltermastering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallterMasterIngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
